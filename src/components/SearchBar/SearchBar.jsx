88044ae6d8173b1bfe3ef952fbbd1d790263e792
montipallarols <montaga.pallarols@gmail.com>
2021-03-10 19:18:31 +0100

    Make search bar area responsive

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,7 @@ import Background from "../../assets/images/bg-shorten-desktop.svg";
 export default function SearchBar() {
   return (
     <div className="search-bar-container">
-      <img className="background" src={Background} alt="background-color" />
+      {/* <img className="background" src={Background} alt="background-color" /> */}
       <div className="search-area">
         <input
           className="search-bar"
