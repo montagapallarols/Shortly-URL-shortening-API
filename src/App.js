ffb66c143e6bc105810d7638d8dcbc76853f32c8
montipallarols <montaga.pallarols@gmail.com>
2021-03-11 12:04:40 +0100

    Add body text

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
+import Body from './components/Body/Body';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SearchBar />
+      <Body/>
     </div>
   );
 }
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,17 @@
+import "./Body.scss";
+
+import React from "react";
+
+export default function Body() {
+  return (
+    <div className="body-container">
+      <div className="body-text">
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+      </div>
+    </div>
+  );
+}
